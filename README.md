6bc776efbac5d9c2f38b6183f9a0ddfc299e5db9
Lukas Fehring <lukasfehring@gmail.com>
2025-01-02 09:48:46 +0100

    Adapt dynamic prior activation function

--- README.md
+++ README.md
@@ -9,7 +9,15 @@ conda create -n DynaBO python-3.10
 conda activate DynaBO
 ```
 
-2. Install our project in an editable state
+2. Clone SMAC and install from fix-priorAcquisitionFunction branch (TODO this should be merged before the release of our codebase)
+```bash
+git clone https://github.com/automl/SMAC3.git
+cd SMAC3
+git checkout 1076-fix-priorAcquisitionFunction
+pip install .
+```
+
+3. Install our project in an editable state
 ```bash
 pip install -e .
 ```
--- dynabo/smac_additions/dynmaic_prior_activation_function.py
+++ dynabo/smac_additions/dynmaic_prior_activation_function.py
@@ -1,12 +1,33 @@
-from smac.acquisition.function import PriorAcquisitionFunction, AbstractAcquisitionFunction, IntegratedAcquisitionFunction, LCB, TS
+from smac.acquisition.function import (
+    PriorAcquisitionFunction,
+    AbstractAcquisitionFunction,
+    IntegratedAcquisitionFunction,
+    LCB,
+    TS,
+)
 from typing import Any
 from ConfigSpace import Configuration
+from smac.model.abstract_model import AbstractModel
+from smac.model.random_forest import AbstractRandomForest
+from smac.utils.logging import get_logger
+from ConfigSpace import ConfigurationSpace
+
+logger = get_logger(__name__)
+
 
 class DynamicPriorAcquisitionFunction(PriorAcquisitionFunction):
+    """
+    Adapt the PriorAcquisitionFunction to enable weighting the prior at any point of the optimization process.
+
+    The existing implemenation is adapted by adding a second configpsace, which contains prior information and adding
+    a `dynamic_init` method to set the prior information, just as the intiial prior.
+    """
+
     def __init__(
         self,
         acquisition_function: AbstractAcquisitionFunction,
         decay_beta: float,
+        prior_configsapce: ConfigurationSpace,
         prior_floor: float = 1e-12,
         discretize: bool = False,
         discrete_bins_factor: float = 10.0,
@@ -16,8 +37,11 @@ class DynamicPriorAcquisitionFunction(PriorAcquisitionFunction):
         self._functions: list[AbstractAcquisitionFunction] = []
         self._eta: float | None = None
 
-        self._hyperparameters: dict[Any, Configuration] | None = None
         self._decay_beta = decay_beta
+        self._prior_configsapce = prior_configsapce
+        self._hyperparameters: dict[Any, Configuration] | None = (
+            self._prior_configsapce.get_hyperparameters()
+        )
         self._prior_floor = prior_floor
         self._discretize = discretize
         self._discrete_bins_factor = discrete_bins_factor
@@ -30,4 +54,60 @@ class DynamicPriorAcquisitionFunction(PriorAcquisitionFunction):
             acquisition_type = self._acquisition_function
 
         self._rescale = isinstance(acquisition_type, (LCB, TS))
-        self._iteration_number = 0
+
+        # Variables needed to adapt the weighting of the prior
+        self._initial_design_size = (
+            None  # The amount of datapoints in the initial design
+        )
+        self._iteration_number = 1  # The amount of configurations the prior was used in the selection of configurations. It starts at 1
+
+    def dynamic_init(
+        self,
+        acquisition_function: AbstractAcquisitionFunction,
+        decay_beta: float,
+        prior_configsapce: ConfigurationSpace,
+        prior_floor: float = 1e-12,
+        discretize: bool = False,
+        discrete_bins_factor: float = 10.0,
+    ) -> None:
+        self._acquisition_function: AbstractAcquisitionFunction = acquisition_function
+        self._functions: list[AbstractAcquisitionFunction] = []
+        self._eta: float | None = None
+
+        self._decay_beta = decay_beta
+        self._prior_configsapce = prior_configsapce
+        self._hyperparameters: dict[Any, Configuration] | None = (
+            self._prior_configsapce.get_hyperparameters()
+        )
+        self._prior_floor = prior_floor
+        self._discretize = discretize
+        self._discrete_bins_factor = discrete_bins_factor
+
+        # check if the acquisition function is LCB or TS - then the acquisition function values
+        # need to be rescaled to assure positiveness & correct magnitude
+        if isinstance(self._acquisition_function, IntegratedAcquisitionFunction):
+            acquisition_type = self._acquisition_function._acquisition_function
+        else:
+            acquisition_type = self._acquisition_function
+
+        self._rescale = isinstance(acquisition_type, (LCB, TS))
+
+        # Variables needed to adapt the weighting of the prior
+        self._initial_design_size = (
+            None  # The amount of datapoints in the initial design
+        )
+        self._iteration_number = 1  # The amount o
+
+    @property
+    def model(self) -> AbstractModel | None:  # noqa: D102
+        return self._model
+
+    @model.setter
+    def model(self, model: AbstractModel) -> None:
+        self._model = model
+        # TODO Adapted by removign the hyperparameter setter
+
+        if isinstance(model, AbstractRandomForest):
+            if not self._discretize:
+                logger.warning("Discretizing the prior for random forest models.")
+                self._discretize = True
