6fa438c2aa6a296d69aeea5f2e7aa98e9afb69ef
Lukas Fehring <lukasfehring@gmail.com>
2025-01-20 14:24:06 +0100

    Update script

--- scripts/extract_gt_priors.py
+++ scripts/extract_gt_priors.py
@@ -1,12 +1,11 @@
 import os
 
-from dynabo.utils.data_utils import build_prior_dataframe, create_prior_data_path, load_df, save_table, save_base_table
+from dynabo.utils.data_utils import build_prior_dataframe, create_prior_data_path, load_df, save_base_table, save_table
 
 if __name__ == "__main__":
     save_base_table()
 
     base_df = load_df()
-    base_df = base_df[base_df.status == "done"]
     for scenario in base_df.scenario.unique():
         scenario_df = base_df[base_df.scenario == scenario]
 
@@ -17,6 +16,9 @@ if __name__ == "__main__":
                 metric_df = dataset_df[dataset_df.metric == metric]
 
                 prior_data_path = create_prior_data_path(scenario, dataset, metric)
-
-                prior_df = build_prior_dataframe(metric_df)
-                save_table(prior_df, os.path.join(prior_data_path, "prior_table.csv"))
+                try:
+                    prior_df = build_prior_dataframe(metric_df)
+                    save_table(prior_df, os.path.join(prior_data_path, "prior_table.csv"))
+                except ValueError as e:
+                    print(f"Error: {e}")
+                    continue
