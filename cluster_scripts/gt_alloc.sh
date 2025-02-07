41bf7d14addeefc59deab6ce877925158a774002
Lukas Fehring <l.fehring@ai.uni-hannover.de>
2025-02-07 13:36:23 +0100

    Aapt run scripts

--- cluster_scripts/gt_alloc.sh
+++ cluster_scripts/gt_alloc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -t 01:00:00
-#SBATCH --array=1-10
+#SBATCH --array=1-300
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu 4G
-#SBATCH -J dynabo-prior
+#SBATCH -J gt
 #SBATCH -p normal
 #SBATCH -A hpc-prf-intexml
 #SBATCH -e /scratch/hpc-prf-intexml/fehring/DynaBO/logs/%x/%A_%a.err
@@ -19,4 +19,4 @@ ml Miniforge3
 source /pc2/users/i/intexml9/.bashrc
 conda activate DynaBO
 
-python dynabo/experiments/prior_experiments/execute_prior_experiments.py
+python dynabo/experiments/gt_experiments/exectue_gt.py
