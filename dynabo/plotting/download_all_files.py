a62c51d400fc957a633d5f029411848afb4d282e
Lukas Fehring <lukasfehring@gmail.com>
2025-02-11 16:01:59 +0100

    Extract difficulty and metadata

--- dynabo/plotting/download_all_files.py
+++ dynabo/plotting/download_all_files.py
@@ -1,9 +1,12 @@
 from py_experimenter.experimenter import PyExperimenter
 
+DATA_GENERATION_CONFIG_PATH = "dynabo/experiments/gt_experiments/config.yml"
 BASELINE_CONFIG_PATH = "dynabo/experiments/baseline_experiments/config.yml"
 PRIOR_EXPERIMENTS_PATH = "dynabo/experiments/prior_experiments/config.yml"
 CREDENTIALS_PATH = "config/database_credentials.yml"
 
+DATA_GENERATION_TABLE_PATH = "plotting_data/datageneration_table.csv"
+DATA_GENERATION_INCUMBENT_PATH = "plotting_data/datageneration_incumbent.csv"
 BASELINE_TABLE_PATH = "plotting_data/baseline_table.csv"
 BASELINE_INCUMBENT_PATH = "plotting_data/baseline_incumbent.csv"
 PRIOR_TABLE_PATH = "plotting_data/prior_table.csv"
@@ -11,11 +14,15 @@ PRIOR_INCUMBENT_PATH = "plotting_data/prior_incumbent.csv"
 PRIOR_PRIORS_PATH = "plotting_data/prior_priors.csv"
 
 if __name__ == "__main__":
-    baseline_experimenter = PyExperimenter(BASELINE_CONFIG_PATH, CREDENTIALS_PATH)
+    data_generation_experimenter = PyExperimenter(DATA_GENERATION_CONFIG_PATH, CREDENTIALS_PATH)
+    # baseline_experimenter = PyExperimenter(BASELINE_CONFIG_PATH, CREDENTIALS_PATH)
     prior_experimenter = PyExperimenter(PRIOR_EXPERIMENTS_PATH, CREDENTIALS_PATH)
 
-    baseline_experimenter.get_table().to_csv(BASELINE_TABLE_PATH, index=False)
-    baseline_experimenter.get_logtable("incumbents").to_csv(BASELINE_INCUMBENT_PATH, index=False)
+    data_generation_experimenter.get_table().to_csv(DATA_GENERATION_TABLE_PATH, index=False)
+    data_generation_experimenter.get_logtable("incumbents").to_csv(DATA_GENERATION_INCUMBENT_PATH, index=False)
+
+    # baseline_experimenter.get_table().to_csv(BASELINE_TABLE_PATH, index=False)
+    # baseline_experimenter.get_logtable("incumbents").to_csv(BASELINE_INCUMBENT_PATH, index=False)
 
     prior_experimenter.get_table().to_csv(PRIOR_TABLE_PATH, index=False)
     prior_experimenter.get_logtable("incumbents").to_csv(PRIOR_INCUMBENT_PATH, index=False)
---
+++ dynabo/plotting/process_data.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from dynabo.plotting.download_all_files import DATA_GENERATION_INCUMBENT_PATH, DATA_GENERATION_TABLE_PATH
+
+
+def load_data():
+    """
+    Load the data from the csv files.
+    """
+    data_generation_table = pd.read_csv(DATA_GENERATION_TABLE_PATH)
+    data_generation_incumbent = pd.read_csv(DATA_GENERATION_INCUMBENT_PATH)
+    data_generation_incumbent = data_generation_incumbent.drop(columns=["ID"])
+
+    return data_generation_table, data_generation_incumbent
+
+
+def process_data(data_generation_table, data_generation_incumbents, epsiolon=0.005):
+    """
+    Process the data and return the processed data.
+    """
+    # Select Final performace not NaN
+
+    joined_data = pd.merge(data_generation_table, data_generation_incumbents, left_on="ID", right_on="experiment_id")
+    joined_data = joined_data[joined_data["final_performance"].notna()]
+
+    joined_data["classification_threshold"] = joined_data["final_performance"] - joined_data["final_performance"] * epsiolon
+
+    # Remove the incumbents
+    joined_data = joined_data[joined_data["performance"] != joined_data["final_performance"]]
+
+    joined_data = joined_data[joined_data["performance"] >= joined_data["classification_threshold"]]
+
+    # For each ID select the lowest "n_trials" in the threshold
+    joined_data = joined_data.sort_values(by=["ID", "n_trials"], ascending=[True, False])
+
+    # Get first value for each ID while preserving ordering
+    joined_data = joined_data.groupby("ID").first().reset_index()
+
+    joined_data = joined_data.reset_index()
+
+    joined_data["hard"] = joined_data["after_n_evaluations"].apply(lambda x: x > 1000)
+    joined_data["medium"] = joined_data["after_n_evaluations"].apply(lambda x: x > 500 and x <= 1000)
+    joined_data["easy"] = joined_data["after_n_evaluations"].apply(lambda x: x > 200 and x <= 500)
+    joined_data["super_easy"] = joined_data["after_n_evaluations"].apply(lambda x: x <= 200)
+
+    joined_data = joined_data[["scenario", "dataset", "after_n_evaluations", "hard", "medium", "easy", "super_easy"]]
+    return joined_data
+
+
+def save_join_data(joined_data: pd.DataFrame, path):
+    joined_data.to_csv(path, index=False)
+
+
+def scenario_plots(joined_data: pd.DataFrame):
+    scenarios = joined_data["scenario"].unique()
+
+    for scenario in scenarios:
+        fig, axs = plt.subplots(1, 2, figsize=(10, 20))
+        relevant_df = joined_data[joined_data["scenario"] == scenario]
+
+        # Bar Plot of Difficulty
+        # Calculate how often hard medium ,...
+        difficulty_counts = relevant_df[["hard", "medium", "easy", "super_easy"]].sum()
+        difficulty_counts = difficulty_counts / difficulty_counts.sum()
+        sns.barplot(x=difficulty_counts.index, y=difficulty_counts.values, ax=axs[0])
+
+        # Violin plot for after_n_evaluations
+        sns.violinplot(
+            y="after_n_evaluations",
+            data=relevant_df,
+            ax=axs[1],
+        )
+
+        fig.suptitle(scenario)
+        plt.savefig(f"plots/metadata/difficulty/{scenario}.pdf")
+        plt.close()
+
+
+if __name__ == "__main__":
+    data_generation_table, data_generation_incumbent = load_data()
+    joined_data = process_data(data_generation_table, data_generation_incumbent)
+    save_join_data(joined_data, "plotting_data/difficulty_groups.csv")
+    scenario_plots(joined_data)
