37297abf1de11f9e404253b4d0b40799c2aef341
Lukas Fehring <lukasfehring@gmail.com>
2025-02-26 14:42:32 +0100

    Updarte plotting

--- dynabo/data_processing/download_all_files.py
+++ dynabo/data_processing/download_all_files.py
@@ -25,25 +25,25 @@ if __name__ == "__main__":
 
     try:
         data_generation_one_seed_experimenter.get_table().to_csv(DATA_GENERATION_ONE_SEED_PATH, index=False)
-        data_generation_one_seed_experimenter.get_logtable("incumbents").to_csv(DATA_GENERATION_INCUMBENT_ONE_SEED_PATH, index=False)
+        data_generation_one_seed_experimenter.get_logtable("configs").to_csv(DATA_GENERATION_INCUMBENT_ONE_SEED_PATH, index=False)
     except Exception:
         print("No DataGeneration One Seed")
 
     try:
         data_generation_medium_hard_experimenter.get_table().to_csv(DATA_GENERATION_MEDIUM_HARD_PATH, index=False)
-        data_generation_medium_hard_experimenter.get_logtable("incumbents").to_csv(DATA_GENERATION_INCUMBENT_MEDIUM_HARD_PATH, index=False)
+        data_generation_medium_hard_experimenter.get_logtable("configs").to_csv(DATA_GENERATION_INCUMBENT_MEDIUM_HARD_PATH, index=False)
     except Exception:
         print("No DataGeneration Medium Hard")
 
     try:
         baseline_experimenter.get_table().to_csv(BASELINE_TABLE_PATH, index=False)
-        baseline_experimenter.get_logtable("incumbents").to_csv(BASELINE_INCUMBENT_PATH, index=False)
+        baseline_experimenter.get_logtable("configs").to_csv(BASELINE_INCUMBENT_PATH, index=False)
     except Exception:
         print("No Baseline")
 
     try:
         prior_experimenter.get_table().to_csv(PRIOR_TABLE_PATH, index=False)
-        prior_experimenter.get_logtable("incumbents").to_csv(PRIOR_INCUMBENT_PATH, index=False)
+        prior_experimenter.get_logtable("configs").to_csv(PRIOR_INCUMBENT_PATH, index=False)
         prior_experimenter.get_logtable("priors").to_csv(PRIOR_PRIORS_PATH, index=False)
     except Exception:
         print("No Prior")
