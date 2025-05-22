4d0c39ab34637e0a969e73f9c1ca58dd59bb8b4a
Lukas Fehring <lukasfehring@gmail.com>
2025-05-22 11:09:12 +0200

    Update

--- cluster_scripts/gt_alloc.sh
+++ cluster_scripts/gt_alloc.sh
@@ -5,18 +5,18 @@
 #SBATCH --mem-per-cpu 4G
 #SBATCH -J gt
 #SBATCH -p normal
-#SBATCH -A hpc-prf-intexml
-#SBATCH -e /scratch/hpc-prf-intexml/fehring/DynaBO/logs/%x/%A_%a.err
-#SBATCH -o /scratch/hpc-prf-intexml/fehring/DynaBO/logs/%x/%A_%a.out
+#SBATCH -A ####
+#SBATCH -e /scratch/####/####/DynaBO/logs/%x/%A_%a.err
+#SBATCH -o /scratch/####/####/DynaBO/logs/%x/%A_%a.out
 
 
-cd /scratch/hpc-prf-intexml/fehring/DynaBO
+cd /scratch/####/####/DynaBO
 
 conda init bash
 ml lang
 ml Miniforge3
 
-source /pc2/users/i/intexml9/.bashrc
+source /####/####/i/####/.bashrc
 conda activate DynaBO
 
 python dynabo/experiments/gt_experiments/exectue_gt.py
