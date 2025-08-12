6b38c5cfba5d189284fbcad013e1aa065d71de2b
Lukas Fehring <l.fehring@ai.uni-hannover.de>
2025-08-12 13:52:54 +0200

    Update run scripts

--- cluster_scripts/ablate_priors.sh
+++ cluster_scripts/ablate_priors.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH -t 24:00:00
-#SBATCH --array=1-6
+#SBATCH -t 01:00:00
+#SBATCH --array=1-300
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu 6G
-#SBATCH -J baseline-alloc
+#SBATCH -J dynabo-prior
 #SBATCH -e /mnt/home/lfehring/DynaBO/logs/%x/%A_%a.err
 #SBATCH -o /mnt/home/lfehring/DynaBO/logs/%x/%A_%a.out
 
+
 cd /mnt/home/lfehring/DynaBO
 
-sleep $(( RANDOM % 150 ))
+sleep $(( RANDOM % 200 ))
 
 conda init bash
 ml lang
@@ -18,4 +19,4 @@ ml Miniforge3
 source /mnt/home/lfehring/.bashrc
 conda activate DynaBO
 
-/mnt/home/lfehring/DynaBO/.venv/bin/python dynabo/experiments/baseline_experiments/execute_baseline.py
+/mnt/home/lfehring/DynaBO/.venv/bin/python dynabo/experiments/ablate_all_priors/ablate_priors_experiements.py
