3402cdb14576ce84be41f6a7cbc92f9ac2d1cb91
Lukas Fehring <l.fehring@ai.uni-hannover.de>
2025-08-27 10:49:41 +0200

    Adapt cluster incumbents

--- dynabo/data_processing/cluster_incumbents.py
+++ dynabo/data_processing/cluster_incumbents.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import PCA
+from gower import gower_matrix
 
 from dynabo.utils.data_utils import extract_dataframe_from_column_and_after_n_evaluations, load_df, save_base_table
 
 
-def execute_clustering(benchmark_name: str, table_name: str, only_incumbent: bool = False):
+def execute_clustering(benchmark_name: str, table_name: str, only_incumbent: bool, use_pca: bool ):
     scenario_dfs = prepare_data(benchmark_name, table_name, only_incumbent)
     for scenario, df in scenario_dfs.items():
         print(scenario)
-        cluster_incumbents(df, scenario)
+        cluster_incumbents(df, scenario, use_pca=use_pca)
 
 
 def prepare_data(benchmark_name: str, table_name: str, only_incumbent: bool = False):
@@ -31,16 +32,25 @@ def prepare_data(benchmark_name: str, table_name: str, only_incumbent: bool = Fa
     return scenario_dfs
 
 
-def cluster_incumbents(df: pd.DataFrame, scenario: str):
+def cluster_incumbents(df: pd.DataFrame, scenario: str, use_pca: bool):
     # Use DBSCAN to cluster the data
     cluster_df = df.copy()
     cluster_df = cluster_df.drop(columns=["cost"])
 
-    dbscan = DBSCAN(eps=0.3, min_samples=5)
-    labels = dbscan.fit_predict(cluster_df)
+    distance_matrix = gower_matrix(cluster_df) 
+    
+    agg = AgglomerativeClustering(
+        n_clusters=20,
+        linkage="average"
+    )
+    labels_agg = agg.fit_predict(distance_matrix)
 
-    pca = PCA(n_components=2)
-    reduced_data = pca.fit_transform(df)
+    if use_pca:
+        pca = PCA(n_components=2)
+        reduced_data = pca.fit_transform(df)
+    else:
+        df = df.drop(columns=["config_lr_power" , "config_opt_momentum"])
+        reduced_data = df.to_numpy()
 
     min_size = 10
     max_size = 300
@@ -57,7 +67,7 @@ def cluster_incumbents(df: pd.DataFrame, scenario: str):
         reduced_data[:, 0],
         reduced_data[:, 1],
         df["cost"] * -1,  # Using positive cost for a more intuitive Z-axis
-        c=labels,
+        c=labels_agg,
         cmap="viridis",
         s=sizes,
     )
@@ -67,15 +77,19 @@ def cluster_incumbents(df: pd.DataFrame, scenario: str):
     ax.add_artist(legend1)
 
     # Set titles and labels
-    ax.set_title("DBSCAN Clustering with PCA")
-    ax.set_xlabel("PCA Dimension 1")
-    ax.set_ylabel("PCA Dimension 2")
+    ax.set_title("DBSCAN Clustering")
+    if use_pca:
+        ax.set_xlabel("Dimension 1")
+        ax.set_ylabel("Dimension 2")
+    else:
+        ax.set_xlabel(df.columns[0])
+        ax.set_ylabel(df.columns[1])
     ax.set_zlabel("Cost")
 
-    plt.savefig(f"dbscan_clustering_pca_{scenario}.png")
+    plt.savefig(f"dbscan_clustering_{scenario}.png")
     plt.close()
-    print(f"Plot saved to dbscan_clustering_pca_{scenario}.png")
+    print(f"Plot saved to dbscan_clustering_{scenario}.png")
 
 
 if __name__ == "__main__":
-    execute_clustering(benchmark_name="mfpbench", table_name="data_generation_pd1", only_incumbent=True)
+    execute_clustering(benchmark_name="mfpbench", table_name="data_generation_pd1", only_incumbent=True, use_pca=False)
